633728f73d42775ccb9998a045fa11cc61784aed
Vitor Alves Pereira <pereira.alves.vitor@outlook.com>
2020-08-06 20:53:59 -0300

    :fire: Dia 04 em andamento

---
+++ src/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const server = express()
+
+server.use(express.static("public"))
+
+.get("/", (req, res) => {
+    return res.sendFile(__dirname + "/views/index.html")
+})
+.get("/study", (req, res) => {
+    return res.send("Study")
+})
+
+.listen(5500)
