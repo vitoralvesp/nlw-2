f278a6324f6e7099fdf6e0c276dbc7ff1dab5aad
Vitor Alves Pereira <pereira.alves.vitor@outlook.com>
2020-08-06 03:05:46 -0300

    /play secret
    Dia 03 da Next Level Week | Rocketseat

---
+++ scripts/addField.js
@@ -0,0 +1,16 @@
+// Comentário em JS
+
+document.querySelector("#add-time")
+.addEventListener("click",cloneField)
+
+function cloneField() {
+    const newFieldContainer = document.querySelector(".schedule-item").cloneNode(true)
+
+    const fields = newFieldContainer.querySelectorAll("input")
+    
+    fields.forEach(function(field) {
+        field.value=""
+    })
+
+    document.querySelector("#schedule-item").appendChild(newFieldContainer)
+}
